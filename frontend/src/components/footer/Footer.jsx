945eb345874c6a2bbd6c94f4d330622910f90c06
Abdullah Sharifi <ab.sharifi19@gmail.com>
2023-10-30 11:44:11 +0100

    change the github link in footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <p>Copyright © 2023 Player2Player - Todos los derechos reservados</p>
-            <a href="https://github.com/AlejandroPachec/Player2Player" rel='noreferrer' target='_blank' title='Github'>
+            <a href="https://github.com/ab-sharifi21/Player2Player-Marketplace" rel='noreferrer' target='_blank' title='Github'>
                 <img src={github} alt="link al repositorio del github" /></a>
         </footer>
     );
